fb2d06acd75770469c7547e878e22cae89f42733
alon eitan <aloni.eitan@gmail.com>
2022-06-20 23:04:58 +0300

    Date format

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -28,7 +28,7 @@ public class ToDoList implements Cloneable, TaskIterable{
         int size = tasks.size();
         for (int i = 0; i < size; i++) {
             if (tasks.get(i) != null){
-                String toAppend = tasks.get(i).toString()+",";
+                String toAppend = "(" + tasks.get(i).toString()+"),";
                 toReturn.append(toAppend);
                 if (i == size- 1){ //last one
                     toReturn.append("]");
